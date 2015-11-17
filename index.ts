8b2069f85b1926c4be3333361264c561b7c904c2
Ryohei Ikegami <iofg2100@gmail.com>
2015-11-17 11:32:41 +0900

    Add buttons

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-document.body.innerHTML = "Hello!";
+
+const messageArea = document.getElementById("message");
+const greetButton = document.getElementById("greet");
+const askBirthdayButton = document.getElementById("askBirthday");
+
+function setMessage(message: string) {
+  messageArea.innerHTML = message;
+}
+
+greetButton.addEventListener("click", () => {
+  setMessage("Hello!");
+});
+askBirthdayButton.addEventListener("click", () => {
+  setMessage(new Date().toString());
+});
