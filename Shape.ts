1c222db4a68709c7cc816cf6ba9c65b83f40662d
Ryohei Ikegami <iofg2100@gmail.com>
2015-11-18 19:21:50 +0900

    Add Shape interface

---
+++ Shape.ts
@@ -0,0 +1,6 @@
+interface Shape {
+  x: number;
+  y: number;
+  draw(ctx: CanvasRenderingContext2D): void;
+}
+export = Shape;
--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
+import Shape = require("./Shape");
+
 const canvas = document.createElement("canvas");
 canvas.width = 800;
 canvas.height = 600;
 document.body.appendChild(canvas);
 
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "#abc";
-ctx.rect(100, 100, 200, 200);
-ctx.fill();
+
+function fillShape(shape: Shape) {
+  ctx.fillStyle = "#abc";
+  shape.draw(ctx);
+  ctx.fill();
+}
+
+const rect = {
+  x: 100, y: 200, w: 100, h: 100,
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.rect(this.x, this.y, this.w, this.h);
+  }
+};
+
+fillShape(rect);
